71dc6cd50b735510a85a140375032cb007f3a991
Frozen Feelings <kiprutohcollo@gmail.com>
2020-06-10 07:41:42 +0300

    imported my app and initiated my url patterns in urls.py

--- Reviews/urls.py
+++ Reviews/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+
+from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth import views
+from registration.backends.simple.views import RegistrationView
+from awwards.forms import RegisterForm
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'', include('awwards.urls')),
+    url(r'^accounts/register/$',
+        RegistrationView.as_view(
+            form_class=RegisterForm
+        ),
+        name='registration_register',
+    ),
+    url(r'^accounts/', include('registration.backends.simple.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'}), 
+    url(r'^tinymce/', include('tinymce.urls')),
+
+
 ]
